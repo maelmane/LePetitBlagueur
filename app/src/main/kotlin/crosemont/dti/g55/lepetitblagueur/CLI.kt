9afd8e88d7e9856fba2baa315142367a6d6cb618
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-09-14 13:58:23 -0400

    Rendu les réponses indifférentes à la casse

--- app/src/main/kotlin/crosemont/dti/g55/lepetitblagueur/CLI.kt
+++ app/src/main/kotlin/crosemont/dti/g55/lepetitblagueur/CLI.kt
@@ -3,23 +3,32 @@ package crosemont.dti.g55.lepetitblagueur
 import java.util.Scanner
 import java.io.PrintStream
 
-class CLI(val flot_entrée : Scanner = Scanner(System.`in`), val flot_sortie : PrintStream = System.out ) : InterfaceUtilisateur {
-	override fun questionner( énoncé : String, réponses : Array<String> ): String{
+class CLI( val flot_entrée : Scanner = Scanner( System.`in` ),
+		   val flot_sortie : PrintStream = System.out ) : InterfaceUtilisateur {
+
+	override fun questionner( énoncé : String, réponses : Array<String> ): String {
 		val scan = flot_entrée
-		afficher( énoncé )
+		var réponseValidée: String? = null
 
-		var réponse = scan.nextLine()
-		while( !(réponse in réponses )){
-			afficher( "«$réponse» n'est pas une réponse comprise." )
+		while( réponseValidée == null ) {
 			afficher( énoncé )
+			var réponse = scan.nextLine()
+			réponseValidée = validerRéponse( réponse, réponses )
 
-			réponse = scan.nextLine()
+			if( réponseValidée == null ) {
+				afficher( "«$réponse» n'est pas une réponse comprise." )
+			}
 		}
 
-		return réponse
+		return réponseValidée
 	}
-	
-	override fun afficher( message : String ){
+
+	fun validerRéponse( réponse: String, réponses: Array<String> ) : String? {
+		var réponse_normalisée = réponse.lowercase().trim()
+		return if ( réponse_normalisée in réponses.map( { it.lowercase().trim() } ) ) réponse_normalisée else null
+	}
+
+	override fun afficher( message : String ) {
 		flot_sortie.println( message )
 	}
 }
